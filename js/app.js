09de60dc8c48a11c8bbd3a22af09e532d7b6e3a9
michele <micheleepedrotti@gmail.com>
2021-04-07 16:06:09 +0200

    slider ok

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 function init() {
+
+    new Vue({
+
+        el: '#app',
+        data:{
+            'imgNum': 1,
+            'sliderLimit': 4
+        },
+        methods:{
+
+            nextImg: function() {
+
+                if (this.imgNum == this.sliderLimit){
+
+                    this.imgNum = 1
+                }
+                else{
+                    this.imgNum++
+                }
+            },
+            prevImg: function () {
+
+                if (this.imgNum == 1) {
+
+                    this.imgNum = 4
+                }
+                else {
+                    this.imgNum--
+                }
+            },
+        }
+    });
 };
 
 document.addEventListener('DOMContentLoaded', init);
